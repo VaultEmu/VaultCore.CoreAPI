7da0eb3b81110ef211f7f047f0ba67a5951d3f7d
Chris Butler <chrisbutler@polygonhearts.com>
2022-12-21 21:57:56 +0000

    Added Nullable annotation to return value of IFeatureResolver.GetFeature()

--- CoreAPI/Source/FrontendFeatures/IFeatureResolver.cs
+++ CoreAPI/Source/FrontendFeatures/IFeatureResolver.cs
@@ -8,5 +8,5 @@ public interface IFeatureResolver
 {
     //Gets the concrete feature that implements an interface that derives from IFeature.
     //Will return null if the feature is not provided by the frontend
-    public T GetFeature<T>() where T : IFeature;
+    public T? GetFeature<T>() where T : IFeature;
 }
